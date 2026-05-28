74ee33d39da87aad4af03dede5a0907145f13e3e
Rexezuge <79224316+Rexezuge@users.noreply.github.com>
2026-05-28 06:23:49 -0500

    FIX: SSL Cert & Key File Path

--- overlay/UsagiInit.sh
+++ overlay/UsagiInit.sh
@@ -1,6 +1,6 @@
 #!/UsagiInit
 mkdir -p /tmp/nginx/logs
-/ChordDHT-Node -uri $NODE_URI -tracker-url $TRACKER_URL -listen :8443 -tls-cert /tmp/ssl/selfsigned/server.crt -tls-key /tmp/ssl/selfsigned/server.key -skip-tls-verify &
+/ChordDHT-Node -uri $NODE_URI -tracker-url $TRACKER_URL -listen :8443 -tls-cert /etc/ssl/selfsigned/server.crt -tls-key /etc/ssl/selfsigned/server.key -skip-tls-verify &
 /usr/local/bin/cloudflared tunnel --no-autoupdate run --token $CLOUDFLARE_TOKEN > /dev/null &
 /usr/local/bin/v2ray run -config /etc/v2ray/config.json > /dev/null &
 /usr/sbin/nginx -p /tmp/nginx -c /etc/nginx/nginx.conf -g "daemon off;" > /dev/null &
