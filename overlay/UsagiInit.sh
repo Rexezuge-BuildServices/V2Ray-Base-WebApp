bd3646a9107b1384f5a5b6d1c54b59b8d97f3abf
Rexezuge <79224316+Rexezuge@users.noreply.github.com>
2026-05-28 05:16:44 -0500

    FIX: Prints the Logs from ChordDHT

--- overlay/UsagiInit.sh
+++ overlay/UsagiInit.sh
@@ -1,6 +1,6 @@
 #!/UsagiInit
 mkdir -p /tmp/nginx/logs
-/ChordDHT-Node -tls-cert /tmp/ssl/selfsigned/server.crt -tls-key /tmp/ssl/selfsigned/server.key > /dev/null &
+/ChordDHT-Node -tls-cert /tmp/ssl/selfsigned/server.crt -tls-key /tmp/ssl/selfsigned/server.key &
 /usr/local/bin/cloudflared tunnel --no-autoupdate run --token $CLOUDFLARE_TOKEN > /dev/null &
 /usr/local/bin/v2ray run -config /etc/v2ray/config.json > /dev/null &
 /usr/sbin/nginx -p /tmp/nginx -c /etc/nginx/nginx.conf -g "daemon off;" > /dev/null &
